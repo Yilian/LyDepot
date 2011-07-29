9df9b77bb31836acde774e9487cbf0e1309ce985
Yilian <Yilian.ly@gmail.com>
2011-07-29 17:32:42 +0800

    Finished Golobalization of Stage2

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+=begin
+Depot::Application.configure do
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => 587,
+    :domain         => "gmail.com",
+    :authentication => "plain",
+    :user_name      => "PragmaticStore",
+    :password       => "web123456",
+    :enable_starttls_auto => true
+  }
+end
+=end
